e691779d9c7b70394a514fe6d6b381f214a17f8b
Gerastenok Konstantin <frankgover@rambler.ru>
2019-11-30 17:55:31 +0000

    Upload New File

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace AutoRun
+{
+    public static class Program : Object
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThreadAttribute]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(AutoRun.Instance);
+        }
+    }
+}
