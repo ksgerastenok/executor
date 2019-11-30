7018be4696ea269d670470853424b8fb2e393134
Gerastenok Konstantin <frankgover@rambler.ru>
2019-11-30 17:52:20 +0000

    Upload New File

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyCultureAttribute("")]
+[assembly: AssemblyTrademarkAttribute("")]
+[assembly: AssemblyDescriptionAttribute("")]
+[assembly: AssemblyConfigurationAttribute("")]
+[assembly: AssemblyTitleAttribute("AutoRun")]
+[assembly: AssemblyProductAttribute("AutoRun")]
+[assembly: AssemblyCompanyAttribute("Microsoft")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Microsoft 2015")]
+
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisibleAttribute(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: GuidAttribute("d68222dc-c390-478a-8503-79dbadfb9040")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
